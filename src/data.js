5c3a50b73bea851c7a7755fa62a1c52aaecc0242
FemFX <ignedko34@gmail.com>
2019-09-15 07:44:22 +0300

    Data file

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        image : 'https://agasta-shop.ru/upload/iblock/8a3/8a3019d7f7a398a35be47dbcf0407505.jpg',
+        title : 'Title',
+        description : "Description",
+        price : 10,
+    },
+    {
+        image : 'https://agasta-shop.ru/upload/iblock/8a3/8a3019d7f7a398a35be47dbcf0407505.jpg',
+        title : 'Title',
+        description : "Description",
+        price : 10,
+    },
+    {
+        image : 'https://agasta-shop.ru/upload/iblock/8a3/8a3019d7f7a398a35be47dbcf0407505.jpg',
+        title : 'Title',
+        description : "Description",
+        price : 10,
+    },
+    {
+        image : 'https://agasta-shop.ru/upload/iblock/8a3/8a3019d7f7a398a35be47dbcf0407505.jpg',
+        title : 'Title',
+        description : "Description",
+        price : 10,
+    },
+]
